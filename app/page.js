08a67156cc4962ebdc0ad16745b639be39c8c726
SajaJawad <saiamaher2002@gmail.com>
2025-12-23 13:02:45 +0200

    Initial Next.js project

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+import { ArrowUpIcon } from "lucide-react";
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+        <div className="flex flex-wrap items-center gap-2 md:flex-row">
+      <Button variant="outline">Button</Button>
+
+    </div>
+    </div>
+  );
+}
